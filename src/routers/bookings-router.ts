8564f896af67e34388c89f9dc22383879398c60d
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-04-30 18:25:18 -0300

    feat: add :bookingId in booking-router

--- src/routers/bookings-router.ts
+++ src/routers/bookings-router.ts
@@ -4,6 +4,10 @@ import { createBooking, findBookings, updateBooking } from '@/controllers';
 
 const bookingRouter = Router();
 
-bookingRouter.all('/*', authenticateToken).get('/', findBookings).post('/', createBooking).put('/', updateBooking);
+bookingRouter
+  .all('/*', authenticateToken)
+  .get('/', findBookings)
+  .post('/', createBooking)
+  .put('/:bookingId', updateBooking);
 
 export { bookingRouter };
